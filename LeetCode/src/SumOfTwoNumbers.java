f9f94a52605d527b178000cfebd7ff2230cc35af
十甫寸木南/SFCMN <wsfcmn@163.com>
2021-09-26 00:04:53 +0800

    [LeetCode] feat: sum of two numbers

---
+++ LeetCode/src/SumOfTwoNumbers.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class SumOfTwoNumbers {
+  public int[] twoSum(int[] nums, int target) {
+    final HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if(map.containsKey(target - nums[i])) {
+        return new int[] {map.get(target - nums[i]), i};
+      }
+      map.put(nums[i], i);
+    }
+    return new int[0];
+  }
+
+  public static void main(String[] args) {
+    final SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
+    final int[] indexesOf9 = sumOfTwoNumbers.twoSum(new int[]{2, 7, 11, 15}, 9);
+    for (int index: indexesOf9) {
+      System.out.println(index);
+    }
+
+    System.out.println("------------------------");
+
+    final int[] indexesOf6 = sumOfTwoNumbers.twoSum(new int[]{3, 2, 4}, 6);
+    for (int index: indexesOf6) {
+      System.out.println(index);
+    }
+  }
+}
